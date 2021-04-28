5366bf9f38b156f050c8cda9b33e02bd6fb28dfe
Chase Simmons <csim6890@gmail.com>
2021-04-28 18:32:52 -0500

    single line string ex added

---
+++ 4-Strings/strings.py
@@ -0,0 +1,9 @@
+# SINGLE LINE COMMENT
+letter = 'P'                
+print(letter)               # P
+print(len(letter))          # 1
+greeting = 'Hello, World!'  
+print(greeting)             
+print(len(greeting))        
+sentence = ("I hope you were expecting another", greeting)
+print(sentence)
