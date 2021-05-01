3eac11681e72974f2774a658ed970afbd523f758
Chase Simmons <csim6890@gmail.com>
2021-04-30 22:14:52 -0500

    more py string is methods

--- 4-Strings/strings.py
+++ 4-Strings/strings.py
@@ -166,3 +166,23 @@ challenge = '30DaysOfPython'
 print(challenge.isidentifier()) # False, because it starts with a number
 challenge = 'thirty_days_of_python'
 print(challenge.isidentifier()) # True
+
+# islower():Checks if all alphabets in a string are lowercase
+
+challenge = 'thirty days of python'
+print(challenge.islower()) # True
+challenge = 'Thirty days of python'
+print(challenge.islower()) # False
+
+# isupper(): returns if all characters are uppercase characters
+
+challenge = 'thirty days of python'
+print(challenge.isupper()) #  False
+challenge = 'THIRTY DAYS OF PYTHON'
+print(challenge.isupper()) # True
+
+# isnumeric():Checks numeric characters
+
+num = '10'
+print(num.isnumeric())      # True
+print('ten'.isnumeric())    # False
