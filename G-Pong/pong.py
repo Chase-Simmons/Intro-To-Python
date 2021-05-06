31820af66ffe6ed9359b7a0ed125b6a085e91c04
Chase Simmons <csim6890@gmail.com>
2021-05-05 19:47:34 -0500

    add paddles

--- G-Pong/pong.py
+++ G-Pong/pong.py
@@ -2,9 +2,30 @@ import turtle
 
 win = turtle.Screen();
 win.title("Pong by @Chase")
-win.bgcolor(0, 0, 0)
+win.bgcolor(0.0667, 0.0667, .0941)
 win.setup(width=800, height=600)
 win.tracer(0)
+
+# Paddle A
+paddle_a = turtle.Turtle()
+paddle_a.speed(0);
+paddle_a.shape("square")
+paddle_a.color("white")
+paddle_a.shapesize(stretch_wid=5, stretch_len=1)
+paddle_a.penup()
+paddle_a.goto(-350, 0)
+
+# Paddle B
+paddle_b = turtle.Turtle()
+paddle_b.speed(0);
+paddle_b.shape("square")
+paddle_b.color("white")
+paddle_b.shapesize(stretch_wid=5, stretch_len=1)
+paddle_b.penup()
+paddle_b.goto(350, 0)
+
+# Ball
+
 
 # Main Game Loop
 
