89ed38b109bc52d7e9e477ff6b281a6b00574812
Chase Simmons <csim6890@gmail.com>
2021-05-07 22:49:26 -0500

    init pygame

--- G-SideScroller/sidescroller.py
+++ G-SideScroller/sidescroller.py
@@ -1,5 +1,16 @@
-import pygame
+import pygame, sys
+from pygame.locals import *
 
-print(pygame.ver)
-# pygame.init()
+pygame.init()
 
+WINDOW_SIZE = (1280, 720)
+
+screen = pygame.display.set_mode(WINDOW_SIZE, 0, 32)
+
+while True:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+
+    pygame.display.update()
