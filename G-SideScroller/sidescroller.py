8b8c7112c10acd75c4d4d39cd1d745e28d849c95
Chase Simmons <csim6890@gmail.com>
2021-05-08 00:51:46 -0500

    higher logic collision

--- G-SideScroller/sidescroller.py
+++ G-SideScroller/sidescroller.py
@@ -10,10 +10,13 @@ WINDOW_SIZE = (1280, 720)
 
 screen = pygame.display.set_mode(WINDOW_SIZE, 0, 32)
 
+dirt_img = pygame.image.load('dirt.png')
+
 player_image = pygame.image.load('player.png')
 
 moving_right = False
 moving_left = False
+vertical_momentum = 0;
 
 
 class Cords:
@@ -23,10 +26,40 @@ class Cords:
 
 
 player_location = Cords(50, 50)
-player_y_momentum = 0
 player_bounds = pygame.Rect(player_location.x, player_location.y, player_image.get_width(), player_image.get_height())
 
 test_bounds = pygame.Rect(100, 100, 100, 50)
+
+
+def collision_test(bounds, tiles):
+    hit_list = []
+    for tile in tiles:
+        if bounds.colliderect(tile):
+            hit_list.append(tile)
+        return hit_list
+
+
+def move(bounds, movement, tiles):
+    collision_types = {'top': False, 'bottom': False, 'right': False, 'left': False}
+    bounds.x += movement.x
+    bounds.y += movement.y
+    hit_list = collision_test(bounds, tiles)
+    for tile in hit_list:
+        if movement.x > 0:
+            bounds.right = tile.left
+            collision_types['right'] = True
+        elif movement.x < 0:
+            bounds.left = tile.right
+            collision_types['left'] = True
+    for tile in hit_list:
+        if movement.y > 0:
+            bounds.bottom = tile.top
+            collision_types['bottom'] = True
+        elif movement.y < 0:
+            bounds.top = tile.bottom
+            collision_types['top'] = True
+    return bounds, collision_types
+
 
 while True:
 
@@ -34,11 +67,11 @@ while True:
     screen.blit(player_image, (player_location.x, player_location.y))
 
     if player_location.y > WINDOW_SIZE[1]-player_image.get_height():
-        player_y_momentum = -player_y_momentum
+        vertical_momentum = -vertical_momentum
     else:
-        player_y_momentum += 0.2
+        vertical_momentum += 0.2
 
-    player_location.y += player_y_momentum
+    player_location.y += vertical_momentum
     player_bounds.x = player_location.x
     player_bounds.y = player_location.y
 
