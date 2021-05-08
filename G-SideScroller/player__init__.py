8cfb7e31ca194982b5f4186871f840bee8309aeb
Chase Simmons <csim6890@gmail.com>
2021-05-08 03:12:32 -0500

    switch to object oriented

---
+++ G-SideScroller/player__init__.py
@@ -0,0 +1,7 @@
+class Player:
+    def __init__(self):
+        self.moving_right = False
+        self.moving_left = False
+        self.x = 50
+        self.y = 50
+        self.vertical_momentum = 0;
--- G-SideScroller/sidescroller.py
+++ G-SideScroller/sidescroller.py
@@ -1,34 +1,23 @@
-import pygame, sys
+import pygame, sys, player__init__
 from pygame.locals import *
 
-clock = pygame.time.Clock()
+
+player = player__init__.Player()
 
 pygame.init()
-pygame.display.set_caption('SideScroller')
-
+clock = pygame.time.Clock()
+pygame.display.set_caption('Side Scroller')
 WINDOW_SIZE = (1280, 720)
-
 screen = pygame.display.set_mode(WINDOW_SIZE, 0, 32)
-
 dirt_img = pygame.image.load('dirt.png')
-
 player_image = pygame.image.load('player.png')
 
-moving_right = False
-moving_left = False
-vertical_momentum = 0;
 
+print(player.moving_right)
 
-class Cords:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
+player_bounds = pygame.Rect(player.x, player.y, player_image.get_width(), player_image.get_height())
 
-
-player_location = Cords(50, 50)
-player_bounds = pygame.Rect(player_location.x, player_location.y, player_image.get_width(), player_image.get_height())
-
-test_bounds = pygame.Rect(100, 100, 100, 50)
+test_bounds = pygame.Rect(0, 600, 1200, 50)
 
 
 def collision_test(bounds, tiles):
@@ -64,21 +53,21 @@ def move(bounds, movement, tiles):
 while True:
 
     screen.fill((146, 244, 255))
-    screen.blit(player_image, (player_location.x, player_location.y))
+    screen.blit(player_image, (player.x, player.y))
 
-    if player_location.y > WINDOW_SIZE[1]-player_image.get_height():
-        vertical_momentum = -vertical_momentum
+    if player.y > WINDOW_SIZE[1]-player_image.get_height():
+        player.vertical_momentum = -player.vertical_momentum
     else:
-        vertical_momentum += 0.2
+        player.vertical_momentum += 0.2
 
-    player_location.y += vertical_momentum
-    player_bounds.x = player_location.x
-    player_bounds.y = player_location.y
+    player.y += player.vertical_momentum
+    player_bounds.x = player.x
+    player_bounds.y = player.y
 
-    if moving_right == True:
-        player_location.x += 4
-    if moving_left == True:
-        player_location.x -= 4
+    if player.moving_right == True:
+        player.x += 4
+    if player.moving_left == True:
+        player.x -= 4
 
     if player_bounds.colliderect(test_bounds):
         pygame.draw.rect(screen, (255, 0, 0), test_bounds)
@@ -91,14 +80,14 @@ while True:
             sys.exit()
         if event.type == KEYDOWN:
             if event.key == K_d:
-                moving_right = True
+                player.moving_right = True
             if event.key == K_a:
-                moving_left = True
+                player.moving_left = True
         if event.type == KEYUP:
             if event.key == K_d:
-                moving_right = False
+                player.moving_right = False
             if event.key == K_a:
-                moving_left = False
+                player.moving_left = False
 
 
     pygame.display.update()
